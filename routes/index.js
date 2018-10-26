c5c1a0c4a6ee0ce5b6cb7e93c4093e723e2cd9f5
Ma7eer <maher.alkendi2@gmail.com>
2018-10-26 13:57:33 -0600

    created checkout functionalities

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+/* setting up enviroment variables */
+require('dotenv/config');
+
+var stripe = require("stripe")(process.env.STRIPE_API_KEY);
+
 var Cart = require('../models/cart');
 
 var Product = require('../models/product');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  var successMsg = req.flash('success')[0];
+  console.log(process.env.STRIPE_API_KEY);
   Product.find(function(err, docs) {
-    res.render('shop/index', { products: docs });
+    res.render('shop/index', { products: docs, successMsg: successMsg });
   });
 });
 
@@ -39,7 +46,29 @@ router.get('/checkout', function(req, res, next) {
     return res.redirect('shop/cart', {products: null});
   }
   var cart = new Cart(req.session.cart);
-  res.render('shop/checkout', { totalPrice: cart.totalPrice});
+  var errMsg = req.flash('error')[0];
+  res.render('shop/checkout', { totalPrice: cart.totalPrice, errMsg: errMsg, noError: !errMsg });
+});
+
+router.post('/checkout', function(req, res, next) {
+  if (!req.session.cart) {
+    return res.redirect('shop/cart', {products: null});
+  }
+  var cart = new Cart(req.session.cart);
+  stripe.charges.create({
+    amount: cart.totalPrice * 100,
+    currency: "usd",
+    source: req.body.stripeToken, // obtained with Stripe.js
+    description: "Test Charge"
+  }, function(err, charge) {
+    if(err) {
+      req.flash('error', err.message);
+      res.redirect('/checkout');
+    }
+    req.flash('success', 'Successfully bought product!');
+    req.session.cart = null;
+    res.redirect('/');
+});
 })
 
 module.exports = router;
