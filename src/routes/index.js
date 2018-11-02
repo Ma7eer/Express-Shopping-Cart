64a350d6f75c631fe93a82263b96455e7bee63c7
Ma7eer <maher.alkendi2@gmail.com>
2018-11-02 09:13:00 -0600

    added the ability to remove items from the cart

--- src/routes/index.js
+++ src/routes/index.js
@@ -35,6 +35,24 @@ router.get('/add-to-cart/:id', function(req, res, next) {
   })
 });
 
+router.get('/reduce/:id', function(req, res, next) {
+  var productId = req.params.id;
+  var cart = new Cart(req.session.cart ? req.session.cart : {});
+
+  cart.reduceByOne(productId);
+  req.session.cart = cart;
+  res.redirect('/shopping-cart');
+});
+
+router.get('/remove/:id', function(req, res, next) {
+  var productId = req.params.id;
+  var cart = new Cart(req.session.cart ? req.session.cart : {});
+
+  cart.removeItem(productId);
+  req.session.cart = cart;
+  res.redirect('/shopping-cart');
+});
+
 router.get('/shopping-cart', function(req, res, next) {
   if (!req.session.cart) {
     res.render('shop/cart', {products: null});
