b7910270d0e0ebb9c6b56434a9d8c8448e9ff298
Ma7eer <maher.alkendi2@gmail.com>
2018-11-02 09:37:37 -0600

    added mlab password and user

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,4 +1,4 @@
 module.exports = {
   'local': 'mongodb://localhost:27017/shopping',
-  'mlab': 'mongodb://<dbuser>:<dbpassword>@ds249503.mlab.com:49503/express-shopping-cart'
+  'mlab': 'mongodb://abc123:abc123@ds249503.mlab.com:49503/express-shopping-cart'
 }
